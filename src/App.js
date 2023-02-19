5e2dd1d60bd35644e42b1be3a07f16a755864fec
Icegreeen <flavioaquila16@gmail.com>
2023-02-19 10:32:24 -0300

    add {cart, context and methods}

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import { Navbar } from "./components/Navbar/Navbar"
 
 import { Shop }from "./pages/shop/Shop.jsx";
 import { Cart }from "./pages/cart/Cart.jsx";
+import { ShopContextProvider } from './context/shop-context';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/"  element={<Shop />}/>
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/"  element={<Shop />}/>
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
